705f2b4417643dcf003db265bc5f0844897b4628
Alessandro Todisco <43938971+alessandrotodisco@users.noreply.github.com>
2019-12-01 19:30:58 +0100

    fix

--- Server/config/database.php
+++ Server/config/database.php
@@ -2,7 +2,7 @@
 
 	$servername = "127.0.0.1";
 	$username = "root";
-	$password = "pippo96";
+	$password = ""; // FILL PSW
 	$f_email = 0;
 	// Create connection
 	$conn = new mysqli($servername, $username, $password);
